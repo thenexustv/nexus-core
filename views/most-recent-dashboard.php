217978c63101a16c03dbb3337ad7e1bf7c6c4eda
Ryan Rampersad <ryan.rampersad@gmail.com>
2013-09-17 15:59:32 -0500

    Fixed .gitignore and polished Most Recent dashboard widget
    .gitignore prevented CSS file syncing
    Condensed arrays for Most Recent widget

--- views/most-recent-dashboard.php
+++ views/most-recent-dashboard.php
@@ -1,9 +1,14 @@
-<?php if (!class_exists('Nexus_Core')) exit(); ?>
+<?php
+	if (!class_exists('Nexus_Core')) exit();
+
+	$posts = array( $recent['show'][0], $recent['fringe'][0] );
+
+?>
 
 <div id="most-recent">
 	<div class="inner">
 
-	<?php foreach ($recent['show'] as $post): ?>
+	<?php foreach ($posts as $post): ?>
 		<div class="episode">
 			<h4 class="title-bag"><a href="<?php echo(get_permalink($post['ID'])); ?>"><?php echo(Nexus_Episode::format_episode_title($post['ID'])); ?></a></h4>
 			<div><p><?php echo($post['post_excerpt']); ?></p></div>
@@ -13,34 +18,15 @@
 					<a href="<?php echo(get_edit_post_link($post['ID'])); ?>">Edit</a>
 				</div>
 				<div class="datetime">
-					<?php /*echo(human_time_difference( strtotime($post['post_date']), current_time('timestamp') ) . ' ago');*/ ?>
+					<?php echo Nexus_Utility::human_time_difference( strtotime($post['post_date']), current_time('timestamp') ) . ' ago'; ?>
 				</div>
-
-			</div>
-		</div>
-	<?php endforeach; ?>
-
-	<br class="clear" />
-	<hr  />
-
-	<?php foreach ($recent['fringe'] as $post): ?>
-		<div class="episode">
-			<h4 class="title-bag"><a href="<?php echo(get_permalink($post['ID'])); ?>"><?php echo(Nexus_Episode::format_episode_title($post['ID'])); ?></a></h4>
-			<div><p><?php echo($post['post_excerpt']); ?></p></div>
-			<div class="meta">
 				
-				<div class="edit">
-					<a href="<?php echo(get_edit_post_link($post['ID'])); ?>">Edit</a>
-				</div>
-				<div class="datetime">
-					<?php /*echo(human_time_difference( strtotime($post['post_date']), current_time('timestamp') ) . ' ago');*/ ?>
-				</div>
+				<br class="clear" />
 
 			</div>
 		</div>
+		
 	<?php endforeach; ?>
-
-	<br class="clear" />
 
 	</div>
 </div>
